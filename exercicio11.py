d05d3daddc57fe8bc450be1cd3d722e2569018c9
William Lídio <william.lidio19@gmail.com>
2021-01-18 00:52:59 -0300

    Criação do exercício 11

---
+++ exercicio11.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""
+	11 - Faça um Programa que peça 2 números inteiros e um número real.
+		 Calcule e mostre:
+		 o produto do dobro do primeiro com metade do segundo.
+		 a soma do triplo do primeiro com o terceiro.
+		 o terceiro elevado ao cubo.
+"""
+
+numero1 = input("Digite o primeiro número: ")
+numero2 = input("Digite o segundo número: ")
+

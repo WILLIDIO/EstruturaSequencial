eddd605635511f5db7630016d70c94eb02e22abb
William Lídio <william.lidio19@gmail.com>
2021-01-19 23:53:44 -0300

    finalizando exercício 12

--- exercicio12.py
+++ exercicio12.py
@@ -8,9 +8,11 @@
 
 
 def calculo (altura):
-	calculo = int((72.7*altura) - 58)
+	calculo = float((72.7*altura) - 58)
 	return calculo
 
-altura = input("Digite sua altura: ")
+print ("Use ponto (.) para separar, exemplo: 1.75 ou 1.80")
+altura = float(input("Digite sua altura em metros: "))
 
-print ("Seu peso ideal é: ", calculo(altura))
+#Fiz uma interpolação de strings para mostrar resultado resumido
+print ("Seu peso ideal é: %.2f" % calculo(altura))
